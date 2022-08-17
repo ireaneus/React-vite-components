d1c069c0bdcbd2fc1d938fb4ba7269b8a14e0aff
ireaneus <bwdave@protonmail.com>
2022-08-17 13:46:08 -0600

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,40 @@ import Header from './components/Header';
 import Action from './components/Action';
 import Options from './components/Options';
 
-class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      options: [],
+export default class IndecisionApp extends React.Component {
+  state = {
+    options: [],
+  };
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option),
+    }));
+  };
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid entry';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'this option is already taken';
     };
-  }
-  componentDidMount() {
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option),
+    }));
+  };
+
+  componentDidMount = () => {
     try {
       const json = localStorage.getItem('options');
       const options = JSON.parse(json);
@@ -28,38 +50,13 @@ class IndecisionApp extends React.Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate = (prevProps, prevState) => {
     if (prevState.options.length !== this.state.options.length) {
       const json = JSON.stringify(this.state.options);
       localStorage.setItem('options', json);
     }
   }
-
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option),
-    }));
-  }
-
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum];
-    alert(option);
-  }
-  handleAddOption(option) {
-    if (!option) {
-      return 'Enter valid entry';
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'this option is already taken';
-    }
-
-    this.setState((prevState) => ({
-      options: prevState.options.concat(option),
-    }));
-  }
+  
   render() {
     const subtitle = 'Put your life in the hands of a computer';
 
@@ -80,5 +77,3 @@ class IndecisionApp extends React.Component {
     );
   }
 }
-
-export default IndecisionApp;
